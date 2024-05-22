c8670fb3fcb6f4787ad4106a696e31e27e2db337
buradabhargavi <bhargaviboorada246@gmail.com>
2024-05-22 17:15:39 +0530

    portals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Layout/Header";
 import MealsList from "./Meals/MealsList";
 import Summary from "./Meals/Summary";
+import Cart from "./Cart/Cart";
 function App() {
   return (
     <div>
+      <Cart />
       <Header />
       <Summary />
       <MealsList />
---
+++ src/Cart/Cart.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = () => {
+  const cartItems = [
+    {
+      id: "m1",
+      name: "Sushi",
+      description: "Finest fish and veggies",
+      price: 22.99,
+    },
+    {
+      id: "m2",
+      name: "Schnitzel",
+      description: "A german specialty!",
+      price: 16.5,
+    },
+  ];
+  return (
+    <Modal>
+      <ul className={`${classes["cart-items"]}`}>
+        {cartItems.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>45.32</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+export default Cart;
